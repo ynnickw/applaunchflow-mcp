1d167348dfff5ea7abb805c8bef92178e32ad4c7
ynnickw <yannick.westermann@tum.de>
2026-09-04 18:25:56 +0200

    release: refresh picker diagnostics

--- src/ui/social-graphics-picker.ts
+++ src/ui/social-graphics-picker.ts
@@ -10,7 +10,7 @@ import { fail } from "../tools/utils.js";
 import { pickerToolMeta, registerPickerResource } from "./picker-resource.js";
 
 export const SOCIAL_GRAPHICS_PICKER_URI =
-  "ui://applaunchflow/social-graphics-picker-v6.html";
+  "ui://applaunchflow/social-graphics-picker-v7.html";
 
 export async function createSocialGraphicsPickerResult(
   client: AppLaunchFlowClient,
